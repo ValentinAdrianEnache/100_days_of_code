307e207e64afab992b08314d0a5e12e9602c797c
Vali <valentinadrianenache@gmail.com>
2023-03-16 12:30:40 +0200

    Average Height

--- Day_5/1.average_height.py
+++ Day_5/1.average_height.py
@@ -3,10 +3,23 @@
 # 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+    student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
 
-#Write your code below this row 👇
+# Write your code below this row 👇
 
+# Solution 1:
+# total_height = sum(student_heights)
+# number_of_students = len(student_heights)
+# average_height = round(total_height / number_of_students)
+# print(average_height)
 
+# Solution 2:
+total_height = 0
+number_of_students = 0
+for height in student_heights:
+    total_height += height
+    number_of_students += 1
+average_height = round(total_height / number_of_students)
+print(average_height)
