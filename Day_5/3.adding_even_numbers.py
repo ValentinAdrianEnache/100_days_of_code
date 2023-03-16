d5c92e307dfe93c0d202bc93cad47500257240e4
Vali <valentinadrianenache@gmail.com>
2023-03-16 21:00:59 +0200

    Fizz Buzz

--- Day_5/3.adding_even_numbers.py
+++ Day_5/3.adding_even_numbers.py
@@ -1,1 +1,24 @@
 """Exercise 3 - Adding Even Numbers"""
+
+# Instructions
+# You are going to write a program that calculates the sum
+# of all the even numbers from 1 to 100.
+# Thus, the first even number would be 2 and the last one is 100:
+#
+# i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+#
+# Important, there should only be 1 print statement in your console output.
+# It should just print the final total and not every step of the calculation
+
+# Solution 1:
+even_sum = 0
+for even_number in range(2, 101, 2):
+    even_sum += even_number
+print(even_sum)
+
+# Solution 2:
+# even_sum = 0
+# for even_number in range(1, 101):
+#     if even_number % 2 == 0:
+#         even_sum += even_number
+# print(even_sum)
