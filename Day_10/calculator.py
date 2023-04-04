6150091572e0464b9c530f59f85de7d8af6b60ed
Vali <valentinadrianenache@gmail.com>
2023-04-04 20:48:08 +0300

    updated art calculator

--- Day_10/calculator.py
+++ Day_10/calculator.py
@@ -1,7 +1,10 @@
 """ Calculator """
 
-
 # Calculator
+from art import logo
+import os
+
+
 
 def add(n1, n2):
     """
@@ -63,14 +66,26 @@ operation = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operation:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above:")
-num2 = int(input("What's the second number?: "))
 
-calculation_function = operation[operation_symbol]
-answer = calculation_function(num1, num2)
+def calculator():
+    print(logo)
 
+    num1 = int(input("What's the first number?: "))
+    for symbol in operation:
+        print(symbol)
+    should_continue = True
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number?: "))
+        calculation_function = operation[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
